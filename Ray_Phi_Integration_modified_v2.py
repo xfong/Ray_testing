1a0dd658bf2d54e24c99ba9da759fd64f1fd3f07
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-05-21 11:37:08 +0800

    Tested local run of ray integration implementation

--- Ray_Phi_Integration_modified_v2.py
+++ Ray_Phi_Integration_modified_v2.py
@@ -26,19 +26,16 @@ def get_rho_tuple(number_of_cells):
     return tuple(z)
 
 ##### Internal functions
-#@ray.remote
-def prob_val(phi_min,phi_max,theta_min,theta_max,rho):
-    #f = lambda y, x: sphericalDatapoint(y, x)
-    [prob,error]=dblquad(lambda y, x: (numerix.sin(y) * rho([[numerix.sin(y)*numerix.cos(x)], [numerix.sin(y)*numerix.sin(x)], [numerix.cos(y)]], order=1)),phi_min,phi_max, lambda phi: theta_min, lambda phi: theta_max, epsabs=1e-15)#, epsrel=1e-13)
-    return prob
-
-def task_loop(worker_idx, total_tasks, phi_array, theta_array, rho):
+@ray.remote
+def task_loop(worker_idx, phi_array, theta_array, rho):
     # Each worker will iterate to emulate a new worker working on a new task
     # Done this way to minimize number of actual task creations and
     # destructions which will slow down overall execution
     global_idx = worker_idx
     worker_accum = 0
     nth = len(theta_array)
+    nphi = len(phi_array)
+    total_tasks=(nth-1)*(nphi-1)
     while global_idx < total_tasks:
         # Pick up range over which the task is supposed to perform integration
 
@@ -50,17 +47,17 @@ def task_loop(worker_idx, total_tasks, phi_array, theta_array, rho):
         max_phi = phi[local_phi_idx+1]
         min_theta = theta[local_theta_idx]
         max_theta = theta[local_theta_idx+1]
-        print('----------------------------------------------------------')
-        conv_fac=(180.0/numerix.pi)
-        print(global_idx)
-        print('Phi_idx = ' + str(local_phi_idx))
-        print('Theta_idx = ' + str(local_theta_idx))
-        print('Phi = ' + str(min_phi*conv_fac) + ' to ' + str(max_phi*conv_fac))
-        print('Theta_idx = ' + str(min_theta*conv_fac) + ' to ' + str(max_theta*conv_fac))
-        print('----------------------------------------------------------')
+        #print('----------------------------------------------------------')
+        #conv_fac=(180.0/numerix.pi)
+        #print(global_idx)
+        #print('Phi_idx = ' + str(local_phi_idx))
+        #print('Theta_idx = ' + str(local_theta_idx))
+        #print('Phi = ' + str(min_phi*conv_fac) + ' to ' + str(max_phi*conv_fac))
+        #print('Theta_idx = ' + str(min_theta*conv_fac) + ' to ' + str(max_theta*conv_fac))
+        #print('----------------------------------------------------------')
 
         # Calculate the integral
-        local_probVal = probVal(min_phi, max_phi, min_theta, max_theta, rho)
+        local_probVal, local_error = dblquad(lambda y, x: (numerix.sin(y) * rho([[numerix.sin(y)*numerix.cos(x)], [numerix.sin(y)*numerix.sin(x)], [numerix.cos(y)]], order=1)), min_phi, max_phi, lambda phix: min_theta, lambda phix: max_theta, epsabs=1e-15, epsrel=1e-8)
 
         # Accumulate local integral into worker track of sum
         worker_accum = worker_accum + local_probVal
@@ -73,10 +70,10 @@ def task_loop(worker_idx, total_tasks, phi_array, theta_array, rho):
 if __name__ == '__main__':
     plt.rcParams.update({'font.size':20})
 
-    num_workers=16
+    num_workers=24
     ray.init(num_cpus=num_workers,memory=102005473280,object_store_memory=21474836480)
 
-    # ### Load mesh details from a saved file
+    #### Load mesh details from a saved file
 
     mesh=pickle.load(open("../mesh_details_cellsize_0pt008_extrude_1pt00001.p","rb"))
     gridCoor = mesh.cellCenters
@@ -90,7 +87,7 @@ if __name__ == '__main__':
     number_of_cells=msize[1]
     print('Total number of cells = ' +str(number_of_cells))
 
-    # ### Loading phi values from a saved file
+    #### Loading phi values from a saved file
 
     rho_value = get_rho_tuple(number_of_cells) # get phi values from a previous .vtk file
     rho = CellVariable(name=r"$\Rho$",mesh=mesh,value=rho_value)
@@ -100,14 +97,23 @@ if __name__ == '__main__':
     #                                    Check from here                                                                                 #
     ##########################################################################
 
-    nth=9001
-    nphi=18001
+    nth=180001
+    nphi=90001
     total_tasks=(nth-1)*(nphi-1)
     theta=numerix.linspace(0,numerix.pi,nth)
     phi=numerix.linspace(0,0.5*numerix.pi,nphi)
 
+    # Load shared memory
+    theta_id = ray.put(theta)
+    phi_id = ray.put(phi)
+    rho_id = ray.put(rho)
+
     task_result = []
     for worker_idx in range(num_workers-1):
-        task_result.append(task_loop(worker_idx, total_tasks, phi, theta, rho))
+        task_result.append(task_loop.remote(worker_idx, phi_id, theta_id, rho_id))
+        #task_result.append(task_loop(worker_idx, phi, theta, rho))
 
-    prob_result = numerix.sum(task_result)
+    prob_array = ray.get(task_result)
+    prob_result = numerix.sum(prob_array)
+
+    #prob_result = numerix.sum(task_result)
--- sub_script.py
+++ sub_script.py
@@ -1,9 +1,8 @@
 #!/bin/bash
 #PBS -j oe
-#PBS -l ncpus=16
-#PBS -l mpiprocs=16
+#PBS -l ncpus=25
 #PBS -l mem=96gb
 
 module load singularity
 cd /home/elefongx/proj/fpe/fipy/Ray_testing
-singularity exec /opt/containers/ubuntu/18/fipy_v2_2a.sif python ./Ray_Phi_Integration_modified.py
+singularity exec -e /opt/containers/ubuntu/18/fipy_v2_2a.sif python ./Ray_Phi_Integration_modified_v2.py
